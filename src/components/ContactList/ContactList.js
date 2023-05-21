76d28dc54a7d629b146f0313dfac8357588bdd83
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-21 17:32:56 +0300

    Adds selectFilteredContacts

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,27 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
 import { deleteContact } from 'redux/operations';
-import { selectFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 import { RiUserUnfollowFill } from 'react-icons/ri';
 import { DeleteBtn, Item, List } from "./ContactList.styled";
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
+  const contacts = useSelector(selectFilteredContacts);
+
   const dispatch = useDispatch();
 
-  const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter));
-  const displayContactlist = filter === "" ? contacts : filteredContacts;
-
-  if (!displayContactlist?.length) {
-    return <div>Contacts not found</div>;
-  }
   const onDeleteContact = (contactId) => {
     dispatch(deleteContact(contactId));
   }
   return (
     <List>
-      {displayContactlist.map(contact => (
+      {contacts.map(contact => (
         <Item key={contact.id}>
           <div>
             <span>{contact.name}:</span> <span>{contact.phone}</span>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,21 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(({ name, phone }) =>
+        name.toLowerCase().trim().includes(normalizedFilter) ||
+        phone.trim().includes(normalizedFilter)
+    );
+    return filteredContacts;
+  }
+);
