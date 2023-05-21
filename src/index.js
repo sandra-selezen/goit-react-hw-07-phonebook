3f4b6ce6bf223f90b0656c8ecaeffb9753faf57c
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-21 12:51:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 import './index.css';
 
+const theme = {
+  colors: {
+    whiteColor: "hsl(0, 0%, 100%)",
+    blackColor: "hsl(0, 0%, 0%)",
+  },
+  borderRadius: "5px",
+  duration: "250ms",
+  timingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
