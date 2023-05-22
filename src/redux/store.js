1571a64e4127a1bbae29eda0df0417671ff400b9
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-05-22 17:23:27 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "./contactsSlice";
-import { filtersReducer } from './filterSlice';
+import { contactsApi } from "./contactsAPI";
+// import { contactsReducer } from "./contactsSlice";
+// import { filtersReducer } from './filterSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
-    filter: filtersReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
   },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    contactsApi.middleware,
+  ],
 });
